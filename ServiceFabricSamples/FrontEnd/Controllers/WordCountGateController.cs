5c26f6e2194fe9130ee495457d6b006d12db7e15
bnaya eshet <bnayanet@hotmail.com>
2017-12-28 13:08:39 +0200

    Stateless front-end Stateful back-end

---
+++ ServiceFabricSamples/FrontEnd/Controllers/WordCountGateController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using System.Threading;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+
+// https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-reverseproxy
+
+namespace FrontEnd.Controllers
+{
+    [Route("api/[controller]")]
+    public class WordCountGateController : Controller
+    {
+        private const string STATEFUL_URL = "http://localhost:19081/ServiceFabricSamples/StatefulWebApi/api/WordCount";
+
+        private string BuildUrl(string key, string relativeUrl = null)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            int partition = key.ToLower()[0] - 'a';
+            return BuildUrl(partition, relativeUrl);
+        }
+
+        private string BuildUrl(int partition, string relativeUrl = null)
+        {
+            partition = partition % 26;
+            if (string.IsNullOrEmpty(relativeUrl))
+                return $"{STATEFUL_URL}?PartitionKey={partition}&PartitionKind=Int64Range";
+            else
+                return $"{STATEFUL_URL}/{relativeUrl}?PartitionKey={partition}&PartitionKind=Int64Range";
+        }   
+
+        // GET api/wordcount
+        [HttpGet]
+        public async ValueTask<(string key, int count)[]> Get()
+        {
+            var query = Enumerable.Range(0, 26)
+                            .Select(GetByPartition)
+                            .Select(m => m.AsTask());
+            IEnumerable<(string key, int count)[]> results =
+                await Task.WhenAll(query);
+
+            var merged = from e in results
+                         from item in e ?? Enumerable.Empty<(string key, int count)>()
+                         select item;
+            var result = merged.ToArray();
+            return result;
+        }
+        public async ValueTask<(string key, int count)[]> GetByPartition(int partition)
+        {
+            string url = BuildUrl(partition);
+            using (var http = new HttpClient())
+            {
+                var response = await http.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<(string key, int count)[]>(json);
+                return data;
+            }
+        }
+
+        // GET api/wordcount/a
+        [HttpGet("{key}")]
+        public async ValueTask<(string key, int count)> Get(string key)
+        {
+            string url = BuildUrl(key, key);
+            using (var http = new HttpClient())
+            {
+                var response = await http.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<(string key, int count)>(json);
+                return data;
+            }
+        }
+
+
+        // POST api/wordcount/a
+#pragma warning disable SG0016 // Controller method is vulnerable to CSRF
+        [HttpPost()]
+        public async ValueTask<IActionResult> Post([FromBody]string word)
+        {
+            string url = BuildUrl(word);
+            using (var http = new HttpClient())
+            {
+                var content = new StringContent(word, Encoding.UTF8, "application/json");
+                var response = await http.PostAsync(url, content);
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return StatusCode((int)response.StatusCode, word);
+            }
+        }
+
+        // PUT api/wordcount/a
+        [HttpPut()]
+        public async ValueTask<IActionResult> PutAsync([FromBody]string word)
+        {
+            //HttpClientExtensions.PostAsJsonAsync<string>(word)
+            string url = BuildUrl(word);
+            using (var http = new HttpClient())
+            {
+                var response = await http.PutAsJsonAsync(url, word);
+                //var content = new ObjectContent<string>(word, new JsonMediaTypeFormatter(), "application/json");
+                //var content = new ObjectContent(word, Encoding.UTF8, "application/json");
+                //var response = await http.PutAsync(url, content);
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return StatusCode((int)response.StatusCode, word);
+            }
+        }
+
+        // DELETE api/wordcount/a
+        [HttpDelete("{word}")]
+        public async ValueTask<IActionResult> DeleteAsync(string word)
+        {
+            string url = BuildUrl(word, word);
+            using (var http = new HttpClient())
+            {
+                var response = await http.DeleteAsync(url);
+                if (response.IsSuccessStatusCode)
+                    return Ok();
+                else
+                    return StatusCode((int)response.StatusCode, word);
+            }
+        }
+#pragma warning restore SG0016 // Controller method is vulnerable to CSRF
+    }
+}
---
+++ ServiceFabricSamples/StatefulWebApi/Controllers/WordCountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using System.Threading;
+
+namespace StatefulWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class WordCountController : Controller
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public WordCountController(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        // GET api/wordcount
+        [HttpGet]
+        public async ValueTask<IEnumerable<(string key, int count)>> Get()
+        {
+            var result = new List<(string key, int count)>();
+            var countState = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("Counting").ConfigureAwait(false);
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                IAsyncEnumerable<KeyValuePair<string, int>> query = await countState.CreateEnumerableAsync(tx).ConfigureAwait(false);
+                var enumerator = query.GetAsyncEnumerator();
+                while (await enumerator.MoveNextAsync(CancellationToken.None))
+                {
+                    var cur = enumerator.Current;
+                    result.Add((cur.Key, cur.Value));
+                }
+
+                await tx.CommitAsync().ConfigureAwait(false);
+            }
+            return result;
+        }
+
+        // GET api/wordcount/a
+        [HttpGet("{key}")]
+        public async ValueTask<(string key, int count)> Get(string key)
+        {
+            var countState = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("Counting").ConfigureAwait(false);
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                var response = await countState.TryGetValueAsync(tx, key, LockMode.Default).ConfigureAwait(false);
+                if (response.HasValue)
+                    return (key, response.Value);
+
+                await tx.CommitAsync().ConfigureAwait(false);
+                return (key, response.HasValue ? response.Value : 0);
+            }
+        }
+
+
+        // POST api/wordcount/a
+#pragma warning disable SG0016 // Controller method is vulnerable to CSRF
+        [HttpPost()]
+        public async ValueTask<IActionResult> Post([FromBody]string word)
+        {
+            var countState = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("Counting").ConfigureAwait(false);
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                if (!await countState.TryAddAsync(tx, word.ToLower(), 1).ConfigureAwait(false))
+                    return new BadRequestObjectResult("Already exists (use put verb)");
+
+                await tx.CommitAsync().ConfigureAwait(false);
+            }
+            return Ok();
+        }
+
+        // PUT api/wordcount/a
+        [HttpPut()]
+        public async ValueTask<IActionResult> PutAsync([FromBody]string word)
+        {
+            var countState = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("Counting").ConfigureAwait(false);
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await countState.AddOrUpdateAsync(tx, word.ToLower(), 0, (k, v) => v + 1).ConfigureAwait(false);
+
+                await tx.CommitAsync().ConfigureAwait(false);
+            }
+            return Ok();
+        }
+
+        // DELETE api/wordcount/a
+        [HttpDelete("{word}")]
+        public async ValueTask<IActionResult> DeleteAsync(string word)
+        {
+            var countState = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("Counting").ConfigureAwait(false);
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await countState.TryRemoveAsync(tx, word.ToLower()).ConfigureAwait(false);
+
+                await tx.CommitAsync().ConfigureAwait(false);
+            }
+            return Ok();
+        }
+#pragma warning restore SG0016 // Controller method is vulnerable to CSRF
+    }
+}
